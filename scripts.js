a9f17c7faac37d6144ed0500939e18bb2adb1fce
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2023-02-13 12:48:33 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,19 @@
-function slideshow(){
-	this.i = 0, 	
-	this.images = ["DSC_0014_new.jpg", "DSC_0020_new.jpg"],	
-	this.time = 3000,
+
+var i = 0, 	
+var images = ["DSC_0014_new.jpg", "DSC_0020_new.jpg"],	
+var time = 3000,
 		
-	this.imageAlt = function(i,images,time){
-		document.slide.src = images[i];
+function imageAlt{
+	document.slide.src = images[i];
 	
-		if(i < images.length - 1){
-		  i++; 
-		} else { 
-			i = 0;
-		}
+	if(i < images.length - 1){
+		i++; 
+	} else { 
+		i = 0;
+	}
 
 	setTimeout("imageAlt()", time);	
 		
 	}
-}
 
-
-window.onload=slideshow.imageAlt;
+window.onload=imageAlt;
