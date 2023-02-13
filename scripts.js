105329ff5b6dab84c7e7858c8a18246310d120ff
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2023-02-13 12:41:32 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,21 @@
-var slideshow = {
-	var i = 0, 	
-	var images = ["DSC_0014_new.jpg", "DSC_0020_new.jpg"],	
-	var time = 3000,	
-}
-
-function imageAlt(slideshow){
-	document.slide.src = images[i];
+function slideshow(){
+	this.i = 0, 	
+	this.images = ["DSC_0014_new.jpg", "DSC_0020_new.jpg"],	
+	this.time = 3000,
+		
+	this.imageAlt = function(i,images,time){
+		document.slide.src = images[i];
 	
-	if(i < images.length - 1){
-	  i++; 
-	} else { 
-		i = 0;
-	}
+		if(i < images.length - 1){
+		  i++; 
+		} else { 
+			i = 0;
+		}
 
-	setTimeout("imageAlt()", time);
+	setTimeout("imageAlt()", time);	
+		
+	}
 }
 
-window.onload=imageAlt;
+
+window.onload=slideshow.imageAlt;
